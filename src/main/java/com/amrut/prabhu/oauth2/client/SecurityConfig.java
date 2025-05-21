526aa802bc99734e1018b794c490b6ebf840b03b
nguyenthaidien <45583973+nguyenthaidien@users.noreply.github.com>
2025-05-21 07:05:20 +0700

    aa

--- src/main/java/com/amrut/prabhu/oauth2/client/SecurityConfig.java
+++ src/main/java/com/amrut/prabhu/oauth2/client/SecurityConfig.java
@@ -1,5 +1,4 @@
 package com.amrut.prabhu.oauth2.client;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
